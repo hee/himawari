521fb3e8e4076b4e45736fde82b8d19d6fb8abfb
Hidekazu Kasagi <kasagi@cocolable.co.jp>
2017-08-28 19:32:56 +0900

    RPI.GPIOライブラリのみを利用

--- sensor_led.py
+++ sensor_led.py
@@ -1,6 +1,5 @@
 import time
 import RPi.GPIO as GPIO
-import wiringpi as pi
 
 INTAVAL = 1
 SLEEPTIME = 3
@@ -10,19 +9,17 @@ LED_PIN = 4
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
-
-pi.wiringPiSetupGpio()
-pi.pinMode(led_pin, 1)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 st = time.time() - INTAVAL
 
 while True:
-  print( GPIO.input(SENSOR_PIN))
+  print(GPIO.input(SENSOR_PIN))
   if(GPIO.input(SENSOR_PIN) == GPIO.HIGH) and (st + INTAVAL < time.time()):
     st = time.time()
     print("人を感知しました")
-    pi.digitalWrite(LED_PIN, 1)
+    GPIO.output(LED_PIN, GPIO.HIGH)
   else:
-    pi.digitalWrite(LED_PIN, 0)
+    GPIO.output(LED_PIN, GPIO.LOW)
 
   time.sleep(SLEEPTIME)
